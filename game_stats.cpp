01090af13e1f3992072883f7f6551924c9e6f74d
Simon Sultana <ss063@reedleycollege.edu>
2025-10-08 13:19:04 -0700

    Added second array and compared in main

--- game_stats.cpp
+++ game_stats.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 void showDrive();
 void showYards(int arr[], int size);
+bool areEqual(int arr1[], int arr2[], int size); 
+
 struct Drive {
     int yards;
     int plays;
@@ -12,14 +14,15 @@ struct Drive {
 
 int main() {
     const int SIZE = 10; 
+    /*
     Drive drives[SIZE] = { {67,11}, {98,18}, {60,10}, {94,12}, {35,5}, {25,4},
         {45,6}, {50,7}, {30,5}, {40,6} };
     cout << "Detroit Lions at Baltimore Ravens" << endl
     << "Sept 29, 2025 Game Statistics" << endl;
-
-    //int yards[SIZE] = { 67, 98, 60, 94, 35, 25, 45, 50, 30, 40 };
-    cout << "First drive yards: " << drives[0].yards << endl;
-    //cout << "Epic second drive: " << yards[1] << " yards" << endl;
+    */
+    int yards[SIZE] = { 67, 98, 60, 94, 35, 25, 45, 50, 30, 40 };
+    //cout << "First drive yards: " << drives[0].yards << endl;
+    cout << "Epic second drive: " << yards[1] << " yards" << endl;
     /*
     for (int i = 0; i < SIZE; i++) {
         cout << "Drive " << i + 1 << ": " << yards[i] << " yards" << endl;
@@ -49,16 +52,19 @@ int main() {
     cout << "Longest drive: " << maxYards << " yards" << endl;
   
     int plays[SIZE] = { 11, 18, 10, 12, 5, 4, 6, 7, 5, 6 };
-    */
+   
     for (int i = 0; i < SIZE; i++) {
         cout << "Drive " << i + 1 << ": " << drives[i].plays << " plays, "
             << drives[i].yards<< " yds" << endl;
     }
-    /*
+     */
     cout << "Yards via function: ";
     showYards(yards, SIZE);
     cout << endl;
-    */
+    int yards2[SIZE] = { 67, 98, 60, 94, 35, 25, 45, 50, 30, 40 };
+    cout << "Do drive yards for each array match?"
+        << (areEqual(yards, yards2, SIZE) ? "Yes" : "No") << endl;
+
     return 0;
 }
 
@@ -71,4 +77,11 @@ void showDrive()
 void showYards(int arr[], int size) {
     for (int i = 0; i < size; i++) cout << arr[i] << " ";
     cout << endl;
+}
+
+bool areEqual(int arr1[], int arr2[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (arr1[i] != arr2[i]) return false;
+    }
+    return true;
 }
